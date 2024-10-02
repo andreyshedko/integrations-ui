e64d2985e85a17e23a8705aa612bac620363a626
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2024-10-02 09:56:28 +0200

    Add header, split layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { ReactNode } from "react";
+import Header from "./header";
 
 export default async function LocaleLayout({
   children,
@@ -9,15 +10,13 @@ export default async function LocaleLayout({
   children: ReactNode;
   params: { locale: string };
 }) {
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
-          {children}
+          <Header>{children}</Header>
         </NextIntlClientProvider>
       </body>
     </html>
